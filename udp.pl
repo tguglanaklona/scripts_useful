7d360a573ee7a925901d1ac42493028fb5f430a3
tguglanaklona <tguglanaklona@gmail.com>
2015-05-29 15:06:01 +0300

    m

--- udp.pl
+++ udp.pl
@@ -1,6 +1,5 @@
 #/usr/bin/perl
 
-
 use IO::Socket;
 use strict;
 
@@ -10,12 +9,13 @@ my $sock = IO::Socket::INET->new(
     PeerAddr => '46.101.55.90',
 ) or die "Could not create socket: $!\n";
 
-my $filename = 'udp.message.txt';
+my $filename = $ARGV[0]; #'udp.message.txt';
 open( FILE, '<', $filename ) or die 'Could not open file:  ' . $!;
 
 undef $/;
 my $whole_file = <FILE>;
 
-#$sock->send($whole_file) or die "Send error: $!\n";
+$sock->send($whole_file) or die "Send error: $!\n";
 
-$sock->send("A"x100 . " " . "B"x100 . " " . "C"x100 . "\r\n") or die "Send error: $!\n";
+#$sock->send("A"x100 . " " . "B"x100 . " " . "C"x100 . "\r\nVia") or die 
+"Send error: $!\n";
