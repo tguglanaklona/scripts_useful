10e5fb182ed70f77045e1aff945fa78b453d76c9
tguglanaklona <tguglanaklona@gmail.com>
2015-06-21 22:54:16 +0300

    postrb extended

--- postrb.rb
+++ postrb.rb
@@ -1,2 +1,17 @@
 require 'rest_client'
-res = RestClient.post 'http://poster.decaptcher.com', :source=>File.new('im.jpeg')
+
+def post_captchaSniper(filename)
+  res = RestClient.post 'http://127.0.0.1:80', :source=>File.new(filename)
+  return res
+end
+
+def post_captchaSniper_href(href)
+  fName = 'temp.gif'
+  f = File.new(fName, 'w')
+  if !f
+    return nil
+  end
+  f.write(RestClient.get href)
+  f.close
+  return post_captchaSniper(fName)
+end
