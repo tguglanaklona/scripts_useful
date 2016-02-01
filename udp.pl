7e819c7819470017738e3bc996caf608813f3e45
tguglanaklona <tguglanaklona@gmail.com>
2016-02-01 21:47:38 +0300

    m

--- udp.pl
+++ udp.pl
@@ -6,7 +6,7 @@ use strict;
 my $sock = IO::Socket::INET->new(
     Proto    => 'udp',
     PeerPort => 5060,
-    PeerAddr => '178.62.35.87',
+    PeerAddr => '46.101.245.229',
 ) or die "Could not create socket: $!\n";
 
 my $filename = $ARGV[0]; #'udp.message.txt';
@@ -17,10 +17,10 @@ my $whole_file = <FILE>;
 
 $sock->send($whole_file) or die "Send error: $!\n";
 
-#$sock->send("A"x100 . " " . "B"x100 . " " . "C"x100 . "\r\nVia") or die 
-"Send error: $!\n";
+##$sock->send("A"x100 . " " . "B"x100 . " " . "C"x100 . "\r\nVia") or die "Send error: $!\n";
+#
+#my $answer = <$sock>;
+#print $answer;
 
-my $answer = <$sock>;
-print $answer;
-
+close( FILE );
 $sock->close();
