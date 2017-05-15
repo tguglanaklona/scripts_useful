d52a4e7659008d7604c039ab8d8df5947d2bbfe8
tguglanaklona <tguglanaklona@gmail.com>
2017-05-15 20:58:08 +0300

    Create changemac2.sh
    add NetworkManager cleaning

---
+++ changemac2.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+rm -r /etc/NetworkManager/system-connections/*
+echo "eth0:"
+ifconfig eth0 down
+macchanger -a eth0
+ifconfig eth0 up
+echo "wlan0:"
+ifconfig wlan0 down
+macchanger -a wlan0
+ifconfig wlan0 up
+/etc/init.d/networking restart
