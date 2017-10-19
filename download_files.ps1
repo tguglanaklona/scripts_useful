384313d1f51ddc20cb74175136bc521ff9631e15
tguglanaklona <tguglanaklona@gmail.com>
2017-10-19 11:17:37 +0300

    Update download_files.ps1

--- download_files.ps1
+++ download_files.ps1
@@ -2,7 +2,7 @@ $start_time = Get-Date
 
 For ($i=1; $i -lt 100; $i++){
     $url = "https://google.com/$i.jpg"    
-    $output = "google_$i.jpg"
+    $output = "goopict_$i.jpg"
     
     Invoke-WebRequest -Uri $url -OutFile $output
 }
