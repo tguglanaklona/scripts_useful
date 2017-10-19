29570a7aeb6e2d9428cf4892e217c4272f5ac811
tguglanaklona <tguglanaklona@gmail.com>
2017-10-19 11:17:05 +0300

    Update download_files.ps1

--- download_files.ps1
+++ download_files.ps1
@@ -1,17 +1,9 @@
 $start_time = Get-Date
 
 For ($i=1; $i -lt 100; $i++){
-
-    if ($i -lt 10){
-        $url = "https://deti-online.com/images/raskraski/raskraski-dlja-malchikov--tachki--0$i.jpg"    
-        $output = "tachki\tachki_0$i.jpg"
-    }
-    else{
-        $url = "https://deti-online.com/images/raskraski/raskraski-dlja-malchikov--tachki--$i.jpg"
-        $output = "tachki\tachki_$i.jpg"
-    }
-
+    $url = "https://google.com/$i.jpg"    
+    $output = "google_$i.jpg"
+    
     Invoke-WebRequest -Uri $url -OutFile $output
-
 }
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
